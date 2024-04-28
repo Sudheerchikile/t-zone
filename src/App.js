4492e9064ca3476677ceb93a804c80db15487e9b
Sudheerchikile <129176642+Sudheerchikile@users.noreply.github.com>
2024-04-28 08:16:30 +0300

    files-added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TimeZoneConverter from './components/TimezoneComponent';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <TimeZoneConverter/>
+    </div>
+  );
+}
+
+export default App;
